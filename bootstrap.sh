75b7987e7dc7da2af4e3ff45a78fe1bba8076d69
DarcusGrey <gaurav.toon.dewangan@gmail.com>
2026-07-08 07:34:04 +0000

    Rename and update IP.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,9 @@
     # 1. Install Gateway API CRDs
-kubectl apply --server-side -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.4.1/standard-install.yaml
+kubectl apply --server-side -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.4.1/standard-install.yaml --context cluster1
+
+kubectl get configmap coredns -n kube-system --context cluster1 -o yaml | \
+  sed 's/forward . \/etc\/resolv.conf/forward . 8.8.8.8/' | \
+  kubectl apply --context cluster1 -f -
 
 helm repo add cilium https://helm.cilium.io/
 helm repo update
@@ -18,9 +22,9 @@ else
 fi
 
 # 3. Install ArgoCD Server to Cluster 1
-kubectl create namespace argocd --dry-run=client -o yaml | kubectl apply -f -
-kubectl apply -n argocd --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl create namespace argocd --dry-run=client -o yaml --context cluster1 | kubectl apply -f - --context cluster1
+kubectl apply -n argocd --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml --context cluster1
 
 # 4. Apply your ApplicationSet so ArgoCD deploys Cilium to your clusters
-kubectl apply -f argocd-infra.yaml
+kubectl apply -f argocd-infra.yaml --context cluster1
 
